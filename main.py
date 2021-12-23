7d61ec2621735f1ec8151d5a0cfcf0f2706883c9
Jurgen <jurgenmane@gmail.com>
2021-12-22 20:24:09 -0600

    Add GET/DELETE endpoints for activities

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def index():
 
 @app.route("/person/<int:id>", methods=["GET"])
 def retrive_todo(id):
-    user = utils.get(cur, id)
+    user = utils.get(cur, "person", id)
     if user == None:
         return (jsonify({"error": "resource not found"}), 404)
 
@@ -36,18 +36,54 @@ def retrive_todo(id):
 
 @app.route("/person/<int:id>", methods=["DELETE"])
 def delete_car(id):
-    user = utils.get(cur, id)
+    user = utils.get(cur, "person", id)
     if user == None:
         return (jsonify({"error": "resource not found"}), 404)
 
     cur.execute(f"DELETE FROM person WHERE id = {id}")
-    conn.commit()
+    conn.commit()  # Save the changes on databes
     return jsonify(user)
 
 
-# # create 2 end endponts
-# # get 2 specific users  get request
-# # / users/2
-# # delete request to the same route on top
-# debug=True make the server reload on changes
+@app.route("/person/<int:id>/activities", methods=["GET"])
+def get_person_activities(id):
+    user = utils.get(cur, "person", id)
+    if user == None:
+        return (jsonify({"error": "resource not found"}), 404)
+
+    query = f"SELECT * FROM activity WHERE person_id = {id}"
+    cur.execute(query)
+    records = cur.fetchall()
+    activities = []
+    for row in records:
+        activity = {
+            "id": row[0], "activity_details": row[1], "person_id": row[2]}
+        activities.append(activity)
+
+    return jsonify(activities)
+
+
+@app.route("/person/<int:user_id>/activity/<int:activity_id>", methods=["DELETE"])
+def delete_activity_from_person(user_id, activity_id):
+    user = utils.get(cur, "person", user_id)
+    if user == None:
+        return (jsonify({"error": "resource not found"}), 404)
+
+    activity = utils.get(cur, "activity", activity_id)
+    if activity == None:
+        return (jsonify({"error": "resource not found"}), 404)
+
+    if user["id"] != activity["person_id"]:
+        return (jsonify({"error": "resource not found"}), 404)
+
+    cur.execute(f"DELETE FROM activity WHERE id = {activity_id}")
+    conn.commit()
+    return jsonify(activity)
+
+
+    # # create 2 end endponts
+    # # get 2 specific users  get request
+    # # / users/2
+    # # delete request to the same route on top
+    # debug=True make the server reload on changes
 app.run("localhost", 3000, debug=True)
--- utils.py
+++ utils.py
@@ -1,9 +1,30 @@
-def get(cur, id):
-    cur.execute(f"SELECT * FROM person WHERE id = {id}")
-    records = cur.fetchall()
-    if len(records) != 0:
-        obj = {"id": records[0][0], "name": records[0]
-               [1], "username": records[0][2]}
+# def get(cur, id):
+#     cur.execute(f"SELECT * FROM person WHERE id = {id}")
+#     records = cur.fetchall()  # Its going to rerun a list of tuples
+#     if len(records) != 0:
+#         obj = {"id": records[0][0], "name": records[0]
+#                [1], "username": records[0][2]}
+#         return obj
+
+#     return None
+
+
+def get(cur, table, id):
+    cur.execute(f"SELECT * FROM {table} WHERE id = {id}")
+    records = cur.fetchone()  # Its going to rerun a list of tuples
+    if records != None:
+        if table == "person":
+            obj = {"id": records[0], "name": records[1],
+                   "username": records[2]}
+
+        elif table == "activity":
+            obj = {
+                "id": records[0], "activity_details": records[1], "person_id": records[2]}
+
         return obj
 
     return None
+
+
+# get(cur, "person", "1")
+# get(cur, "activity", "1")
